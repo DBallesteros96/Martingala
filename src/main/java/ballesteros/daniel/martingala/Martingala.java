48b0e87910b8d9dd2ac2c19aebba0092f2a5c4f0
Daniel <daniel.ballesteros.medrano@gmail.com>
2022-11-07 19:16:30 +0100

    Depurado

--- src/main/java/ballesteros/daniel/martingala/Martingala.java
+++ src/main/java/ballesteros/daniel/martingala/Martingala.java
@@ -13,31 +13,31 @@ import java.util.Scanner;
 public class Martingala {
 
     public static void main(String[] args) {
-        
+
         String temporal;
         String ultimoColor;
-        boolean continua = true;
+        boolean continuaJugando = true;
         int contadorApuestas = 0;
 
         int cantidadApuesta;
         String resultadoApuesta;
-        
+
         Scanner teclado = new Scanner(System.in);
-        
+
         System.out.print("Introduzca el nombre del jugador: ");
         String nombreJugador = teclado.nextLine();
-        
-        do{
+
+        do {
             System.out.print("Introduce el saldo inicial: ");
             temporal = teclado.nextLine();
-        }while (!esEntero(temporal));
-        
+        } while (!esEntero(temporal));
+
         int saldo = Integer.parseInt(temporal);
 
         Jugador usuario = new Jugador(nombreJugador, saldo);
         Ruleta ruleta = new Ruleta();
 
-        while (continua) {
+        while (continuaJugando) {
 
             ultimoColor = ruleta.girar();
 
@@ -45,18 +45,20 @@ public class Martingala {
                 if (ultimoColor.equals("Verde")) {
                     continue;
                 } else if (ultimoColor.equals("Negro")) {
+
                     cantidadApuesta = 1;
                     usuario.setColorApuesta("Rojo");
-
                     System.out.println("Han salido dos Negros seguidos.");
                     do {
                         if (usuario.apuesta(cantidadApuesta)) {
                             System.out.println("El usuario ha apostado " + cantidadApuesta + " euros al Rojo.");
                             contadorApuestas += 1;
                         } else {
-                            continua = false;
+                            //No tiene dinero suficiente para doblar la apuesta
+                            continuaJugando = false;
                             break;
                         }
+
                         resultadoApuesta = ruleta.girar();
                         System.out.println("Ha salido " + resultadoApuesta);
                         if (resultadoApuesta == usuario.getColorApuesta()) {
@@ -76,7 +78,8 @@ public class Martingala {
                             System.out.println("El usuario ha apostado " + cantidadApuesta + " euros al Negro.");
                             contadorApuestas += 1;
                         } else {
-                            continua = false;
+                            //No tiene dinero suficiente para doblar la apuesta
+                            continuaJugando = false;
                             break;
                         }
                         resultadoApuesta = ruleta.girar();
@@ -95,17 +98,17 @@ public class Martingala {
         System.out.println("El usuario se ha quedado con " + usuario.getSaldo() + " euros.");
         System.out.println("El usuario ha apostado " + contadorApuestas + " veces.");
     }
-    
-    public static boolean esEntero(String texto){
+
+    public static boolean esEntero(String texto) {
         int valorEntero;
         try {
             valorEntero = Integer.parseInt(texto);
             return true;
-           
-        }catch(NumberFormatException error){
+
+        } catch (NumberFormatException error) {
             System.err.println("Introduzca un número entero.");
             return false;
         }
-        
+
     }
 }
